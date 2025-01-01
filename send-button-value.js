dabdc620e7ac5bb5cbd24a376929fc3260fb0a68
bodhisatva <hmh.uhtala11@gmail.com>
2025-01-01 17:18:15 +0200

    feat(button-value-to-server): xhr to server

---
+++ send-button-value.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  let interval;
+  let value = 0;
+
+  function sendValue() {
+    $.ajax({
+      url: "/server-endpoint-here",
+      method: "POST",
+      data: { value: value },
+      success: function (response) {
+        console.log("Value sent to server:", value, response);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(
+          "Error sending value to server:",
+          textStatus,
+          errorThrown,
+          jqXHR
+        );
+      },
+    });
+  }
+
+  function startSendingValue() {
+    value = 1;
+    $("#value").text(value);
+    sendValue();
+    interval = setInterval(sendValue, 10);
+  }
+
+  function stopSendingValue() {
+    clearInterval(interval);
+    value = 0;
+    $("#value").text(value);
+    sendValue();
+  }
+
+  $("#holdButton").on("mousedown touchstart", function () {
+    startSendingValue();
+  });
+
+  $("#holdButton").on("mouseup touchend mouseleave", function () {
+    stopSendingValue();
+  });
+});
